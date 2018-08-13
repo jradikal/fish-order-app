6fa62c46c81fad8858c65ced09e0db0eca55db64
Jared <jaredjayz@hotmail.com>
2018-08-13 20:20:53 +0100

    added AddFishForm, NotFound and Router. Made changes to App,Inventory,Storepicker and index.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    //stop the form from submitting
+    event.preventDefault();
+    // get the text from the input
+    const storeName = this.myInput.value.value;
+    //change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store → </button>
       </form>
     );
