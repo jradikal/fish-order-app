42a17880b6db9c6d5b881beb872d079811a39002
Jared <jaredjayz@hotmail.com>
2018-08-13 17:32:33 +0100

    added store picker component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker.js";
 
-class StorePicker extends React.Component {}
-
-history;
+render(<StorePicker />, document.querySelector("#main"));
